096d8f5823af17e1369983a0cddacfb90c1348b8
Ata <laythese@gmail.com>
2020-09-04 14:52:56 +0300

    Add files via upload

--- Lab8/Test.java
+++ Lab8/Test.java
@@ -10,34 +10,34 @@ public class Test
         SavingsAccount sa = new SavingsAccount(1,150.0,0.10);
         CheckingAccount ca = new CheckingAccount(2,200.0,1.50);
         
-        sa.withdraw(-100);
-        sa.displayBalance();
-        sa.withdraw(155.0);
-        sa.displayBalance();
+        sa.withdraw(-100); // You can't withdraw negative values //
+        sa.displayBalance(); // Your balance is: 150.0 //
+        sa.withdraw(155.0); // Withdraw amount exceeds account balance //
+        sa.displayBalance(); // Your balance is: 150.0 //
         sa.withdraw(150.0);
-        sa.displayBalance();
+        sa.displayBalance(); // Your balance is: 0.0 //
           
         System.out.println();
         
-        sa.deposit(-1000);
-        sa.displayBalance();
+        sa.deposit(-1000); //You can't deposit negative integer//
+        sa.displayBalance(); // Your balance is: 0.0 //
         sa.deposit(200);
-        sa.displayBalance();
+        sa.displayBalance(); // Your balance is: 220.0 //
         
         System.out.println("------------------------------");
         
-        ca.withdraw(-200);
-        ca.displayBalance();
-        ca.withdraw(210);
-        ca.displayBalance();
+        ca.withdraw(-200); // You can't withdraw negative integer //
+        ca.displayBalance(); // Your balance is: 200.0 //
+        ca.withdraw(210); // Not enough credits //
+        ca.displayBalance(); // Your balance is: 200.0 //
         ca.withdraw(150);
-        ca.displayBalance();
+        ca.displayBalance(); // Your balance is: 48.5 //
         
         System.out.println();
         
-        ca.deposit(-100);
-        ca.displayBalance();
+        ca.deposit(-100); // You can't deposit negative integer //
+        ca.displayBalance(); // Your balance is: 48.5 //
         ca.deposit(200);
-        ca.displayBalance();
+        ca.displayBalance(); // Your balance is: 247.0 //
     }
 }
